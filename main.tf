26c41f420dc51ec07f71d86f4839a0b743ae06f2
M-AKSHAYVALLIYAPPA <am4637@srmist.edu.in>
2025-05-08 03:23:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "tf_instance" {
+  ami           = "ami-0c101f26f147fa7fd"  # Amazon Linux 2023 AMI in us-east-1
   instance_type = "t2.micro"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
